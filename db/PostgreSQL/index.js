51962fa9e58c39e41a2fd0baf343b4e47e1d1644
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-29 16:18:46 -0700

    cleaned up code

--- db/PostgreSQL/index.js
+++ db/PostgreSQL/index.js
@@ -1,45 +1,45 @@
-const fs = require('fs');
-const path = require('path');
-const { Pool, Client } = require('pg');
+const fs = require('fs')
+const path = require('path')
+const { Pool, Client } = require('pg')
 const copyFrom = require('pg-copy-streams').from
-const config = require('./config.json');
+const config = require('./config.json')
 
-let inputFile = path.join(__dirname, '/data/entryData1.csv');
+let inputFile = path.join(__dirname, '/data/entryData1.csv')
 let targetTable = 'home'
 
-const host = config.host;
-const user = config.user;
-const pw = config.pw;
-const db = config.db;
-const port = config.port;
-const conString = `postgresql://${user}:${pw}@${host}:${port}/${db}`;
+const host = config.host
+const user = config.user
+const pw = config.pw
+const db = config.db
+const port = config.port
+const conString = `postgresql://${user}:${pw}@${host}:${port}/${db}`
 const tableString = `CREATE TABLE IF NOT EXISTS home( id INT, type VARCHAR, description VARCHAR, tags VARCHAR, price VARCHAR, location VARCHAR, image VARCHAR, rating SMALLINT, "numRatings" SMALLINT );`
 
 //connecting to Database
 
 const pool = new Pool({
   connectionString: conString
-});
+})
 
 const client = new Client({
   connectionString: conString
 })
 
-client.connect();
+client.connect()
 
 client.query(tableString, (err, res) => {
-  console.log(err, res);
-});
+  console.log(err, res)
+})
 
 const executeQuery = (targetTable) => {
   const execute = (target, callback) => {
     client.query(`Truncate ${target}`, (err) => {
       if (err) {
-        client.end();
-        callback(err);
+        client.end()
+        callback(err)
       } else {
-        console.log(`Truncated ${target}`);
-        callback(null, target);
+        console.log(`Truncated ${target}`)
+        callback(null, target)
       }
     })
   }
@@ -49,20 +49,20 @@ const executeQuery = (targetTable) => {
   execute(targetTable, (err) => {
     if (err) return console.log(`Error in Truncate Table: ${err}`)
     var stream = client.query(copyFrom(`COPY ${targetTable} FROM STDIN DELIMITER '|' CSV`));
-    var fileStream = fs.createReadStream(inputFile);
+    var fileStream = fs.createReadStream(inputFile)
 
     fileStream.on('error', (err) => {
-      console.log(`Error in creating read stream ${err}`);
+      console.log(`Error in creating read stream ${err}`)
     })
     stream.on('error', (err) => {
-      console.log(`Error in creating stream ${err}`);
+      console.log(`Error in creating stream ${err}`)
     })
     stream.on('end', () => {
-      console.log(`Completed loading data into ${targetTable}`);
-      client.end();
+      console.log(`Completed loading data into ${targetTable}`)
+      client.end()
     })
-    fileStream.pipe(stream);
+    fileStream.pipe(stream)
   })
 }
 
-executeQuery(targetTable);
+executeQuery(targetTable)
--- db/PostgreSQL/seedPostgreSQL.js
+++ db/PostgreSQL/seedPostgreSQL.js
@@ -1,37 +1,37 @@
 const fs = require('fs')
-const { Generator } = require('../../helperFn/dataGenerator.js');
+const { Generator } = require('../../helperFn/dataGenerator.js')
 const home = require('os').homedir()
 var path = home + '/Coding/db/data/'
 
-let start = 1;
-let max = 20;
-let current = 1;
-let count = 0;
+let start = 1
+let max = 20
+let current = 1
+let count = 0
 console.time('Data for PostgreSQL completed in');
 
 let seedEntry = (start, max, current) => {
   if (start > max) {
-    console.timeEnd('Data for PostgreSQL completed in');
-    current = 1;
-    return;
+    console.timeEnd('Data for PostgreSQL completed in')
+    current = 1
+    return
   } else {
-    let string = "";
+    let string = ""
     for(let i = 0; i <= 500000; i++) {
       let generatedData = Generator()
       let entry = `${count}|${generatedData.itemID}|${generatedData.name}|${generatedData.price}|${generatedData.store}|${generatedData.returnDays}`
 
-      string += entry + '\n';
+      string += entry + '\n'
       current++
-      count++;
+      count++
     }
 
     fs.appendFile(path + 'entryData1.csv', string, (err) => {
-      if (err) throw err;
-      console.log('Finished loop ' + start);
+      if (err) throw err
+      console.log('Finished loop ' + start)
       start++;
-      seedEntry(start, max, current);
+      seedEntry(start, max, current)
     })
   }
 }
 
-seedEntry(start, max, current);
+seedEntry(start, max, current)
